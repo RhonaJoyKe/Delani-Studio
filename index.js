f1205490c79c49aed73e2bc2d6be09a8362c0e4d
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-09-10 20:46:29 +0300

    add the alert message function

---
+++ index.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function() {
+    jQuery("button#button").click(function() {
+        var userName=$('text').val;
+        var usermail=$('email').val;
+        var usermsg=$('texta').val;
+        if (userName){
+            
+        }
+    })
+});
