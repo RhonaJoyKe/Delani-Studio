d4239ae36845df379797fdef893664245fbaa0de
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-09-11 17:49:37 +0300

    adds the toggling into the design

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 jQuery(document).ready(function() {
-    jQuery("#button").click(function() {
-        var username = $('text').val;
-        var email = $('email').val;
-        var message = $('text').val;
+  $('.desimg').click(function() {
+    console.log("clicked")
+    //$().toggle(function() {
+    $("#destxt").toggle();
+  });
+  });
+
+
+
+
+
+//     jQuery("#button").click(function() {
+//         var username = $('text').val;
+//         var email = $('email').val;
+//         var message = $('text').val;
     
-        if (username.checked) { 
-            console.log("Name is filled");
-        }else {
-            alert("Please enter your name");
-          }
-           if (email.checked) {
-              console.log("Email is filled");}
-              else {
-                alert("Please enter your DOB");
-              }
-              if (message) {
-                console.log("message is selected");
+//         if (username.checked) { 
+//             console.log("Name is filled");
+//         }else {
+//             alert("Please enter your name");
+//           }
+//            if (email.checked) {
+//               console.log("Email is filled");}
+//               else {
+//                 alert("Please enter your DOB");
+//               }
+//               if (message) {
+//                 console.log("message is selected");
         
-            } else {
-                 alert("Please select your gender");
+//             } else {
+//                  alert("Please select your gender");
         
-              }
+//               }
             
-            })
+//             })
         
-});
+// });
 
         
-       
+      
