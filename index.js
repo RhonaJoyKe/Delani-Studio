6366a81993de4fe9982be7e6728ed1e039293221
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-09-12 15:17:02 +0300

    add hover functionality using jquery

--- index.js
+++ index.js
@@ -48,7 +48,52 @@ jQuery(document).ready(function() {
     $(".bg1").hide();
      
     });
-
+  $('#img2').hover(function(){
+    $(".bg2").show();
+       
+    });
+  $('#img2').mouseout(function(){
+    $(".bg2").hide();
+         
+  });
+  $('#img3').hover(function(){
+    $(".bg3").show();
+           
+  });
+  $('#img3').mouseout(function(){
+    $(".bg3").hide();
+             
+  });
+  $('#img4').hover(function(){
+  $(".bg4").show();
+  });
+  $('#img4').mouseout(function(){
+    $(".bg4").hide();
+  });
+  $('#img5').hover(function(){
+    $(".bg5").show();
+  });
+  $('#img5').mouseout(function(){
+    $(".bg5").hide();
+  });
+  $('#img6').hover(function(){
+    $(".bg6").show();
+ });
+  $('#img6').mouseout(function(){
+   $(".bg6").hide();
+  });
+  $('#img7').hover(function(){
+    $(".bg7").show();
+  });
+  $('#img7').mouseout(function(){
+    $(".bg7").hide();
+  });
+  $('#img8').hover(function(){
+    $(".bg8").show();
+  });
+  $('#img8').mouseout(function(){
+   $(".bg8").hide();
+  });
 
 });
 
